93f7761b3e358aa4786bbdcc394c526160688d6d
pandora <pandora_tl@yahoo.com>
2022-06-30 12:35:03 +0300

    задача2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b, c;
+
+Console.Write("Введите певое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+
+if(a > b & a > c ) 
+{
+    Console.Write("Максимальное число " + a);
+} ;
+
+if(b > a & b > c)
+{
+    Console.Write("Максимальное число " + b);
+} ;
+
+if(c > a & c > b)
+{
+    Console.Write("Максимальное число " + c);
+} ;
+
+
+
+
